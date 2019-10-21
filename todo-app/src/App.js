8dabca976c8d7ca5f916eae71cd55b36a7485e17
RonKbS <stephen.bamwite@gmail.com>
2019-10-21 16:57:38 +0000

    stop index from moving to -1

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -51,7 +51,11 @@ function App() {
     if ( i === 0 && todos.length === 1) return;
     setTodos(todos => todos.slice(0, i).concat(todos.slice(i + 1, todos.length)));
     setTimeout(() => {
-      document.forms[0].elements[i - 1].focus();
+      if(i === 0) {
+        document.forms[0].elements[i].focus();
+      } else {
+        document.forms[0].elements[i - 1].focus();
+      }
     }, 0);
   }
 
